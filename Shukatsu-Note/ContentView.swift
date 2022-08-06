f4e148aeed6cbeb5bc8510feeacb64f156ef4541
Haruki Koga <harukikoga.dev@gmail.com>
2022-08-06 21:26:06 +0900

    Initial Commit

---
+++ Shukatsu-Note/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shukatsu-Note
+//
+//  Created by 古賀遥貴 on 2022/08/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shukatsu-Note/Shukatsu_NoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shukatsu_NoteApp.swift
+//  Shukatsu-Note
+//
+//  Created by 古賀遥貴 on 2022/08/06.
+//
+
+import SwiftUI
+
+@main
+struct Shukatsu_NoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
