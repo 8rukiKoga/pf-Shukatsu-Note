adcafc478030e7e3b60b82a313a93d64af0b664c
Haruki Koga <harukikoga.dev@gmail.com>
2022-08-06 22:41:59 +0900

    -ノートView追加

---
+++ Shukatsu-Note/Views/CompanyList/NoteEditView.swift
@@ -0,0 +1,20 @@
+//
+//  NoteEditView.swift
+//  Shukatsu-Note
+//
+//  Created by 古賀遥貴 on 2022/08/06.
+//
+
+import SwiftUI
+
+struct NoteEditView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NoteEditView_Previews: PreviewProvider {
+    static var previews: some View {
+        NoteEditView()
+    }
+}
---
+++ Shukatsu-Note/Views/CompanyList/NoteListView.swift
@@ -0,0 +1,20 @@
+//
+//  NoteListView.swift
+//  Shukatsu-Note
+//
+//  Created by 古賀遥貴 on 2022/08/06.
+//
+
+import SwiftUI
+
+struct NoteListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NoteListView_Previews: PreviewProvider {
+    static var previews: some View {
+        NoteListView()
+    }
+}
