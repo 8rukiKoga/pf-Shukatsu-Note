01d4745ffd020776b54a3ab48d50df601d034230
Haruki Koga <harukikoga.dev@gmail.com>
2022-09-06 10:10:15 +0900

    カスタムカラーを一元管理できるextensionを追加

---
+++ Shukatsu-Note/Views/Extensions/ColorExtension.swift
@@ -0,0 +1,13 @@
+//
+//  ColorExtension.swift
+//  Shukatsu-Note
+//
+//  Created by 古賀遥貴 on 2022/09/06.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let themeColor = Color("ThemeColor")
+}
