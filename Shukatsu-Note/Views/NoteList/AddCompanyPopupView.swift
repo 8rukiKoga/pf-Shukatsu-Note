5f14f6382f5214055c8951bca8394e9fb9aa6421
Haruki Koga <harukikoga.dev@gmail.com>
2022-09-07 18:51:34 +0900

    登録企業数を15個までにした

--- Shukatsu-Note/Views/NoteList/AddCompanyPopupView.swift
+++ Shukatsu-Note/Views/NoteList/AddCompanyPopupView.swift
@@ -10,57 +10,73 @@ import SwiftUI
 struct AddCompanyPopupView: View {
     
     @ObservedObject var companyVm: CompanyViewModel
+    // ポップアップを表示するか
     @Binding var showingPopup: Bool
-    
+    // 登録数限界のアラートを表示するか
+    @State var showingAlert: Bool = false
+    // 登録する企業名
     @State var newCompanyName: String = ""
-    
+    // スマホのスクリーン幅
     private let screenWidth: CGFloat = 290
     
     var body: some View {
-            ZStack {
-                RoundedRectangle(cornerRadius: 7)
-                    .fill(Color(.systemGray5))
-                    .frame(width: screenWidth, height: 180, alignment: .center)
-                VStack {
-                    Spacer()
-                    Text("Add Company")
-                        .font(.headline)
-                    Text("企業名を入力してください")
-                        .font(.subheadline)
-                    Spacer()
-                    TextField("例) さんぷる株式会社", text: $newCompanyName)
-                        .padding(.vertical)
-                        .padding(.horizontal, 5)
-                        .frame(width: 230, height: 25)
-                        .background(Color(.systemBackground))
-                        .cornerRadius(7)
-                    Spacer()
+        ZStack {
+            RoundedRectangle(cornerRadius: 7)
+                .fill(Color(.systemGray5))
+                .frame(width: screenWidth, height: 180, alignment: .center)
+            VStack {
+                Spacer()
+                Text("Add Company")
+                    .font(.headline)
+                Text("企業名を入力してください")
+                    .font(.subheadline)
+                
+                Text("登録数: \(companyVm.companyList.count)/15")
+                    .font(.caption2)
+                    .foregroundColor(companyVm.companyList.count == 15 ? .red : .gray)
+                Spacer()
+                TextField("例) さんぷる株式会社", text: $newCompanyName)
+                    .padding(.vertical)
+                    .padding(.horizontal, 5)
+                    .frame(width: 230, height: 25)
+                    .background(Color(.systemBackground))
+                    .cornerRadius(7)
+                Spacer()
+                
+                Divider()
+                
+                HStack {
+                    Button {
+                        showingPopup = false
+                    } label: {
+                        Text("キャンセル")
+                            .frame(width: screenWidth / 2)
+                    }
                     
-                    Divider()
-                    
-                    HStack {
-                        Button {
-                            showingPopup = false
-                        } label: {
-                            Text("キャンセル")
-                                .frame(width: screenWidth / 2)
-                        }
-                        
-                        Button {
+                    Button {
+                        if companyVm.companyList.count <= 15 {
+                            // 登録企業が15個以下の場合
                             companyVm.addCompany(name: newCompanyName)
                             showingPopup = false
-                        } label: {
-                            Text("保存")
-                                .fontWeight(.bold)
-                                .frame(width: screenWidth / 2)
+                        } else {
+                            // 登録企業が15個(以上)の場合
+                            showingAlert = true
                         }
+                    } label: {
+                        Text("保存")
+                            .fontWeight(.bold)
+                            .frame(width: screenWidth / 2)
                     }
-                    .padding(3)
-                    
                 }
-                .frame(width: screenWidth, height: 160)
+                .padding(3)
+                
             }
-            .frame(width: screenWidth, height: 180, alignment: .center)
+            .frame(width: screenWidth, height: 160)
+            .alert(isPresented: $showingAlert) {
+                Alert(title: Text("企業の登録は15個までです🙇🏻‍♂️"))
+            }
+        }
+        .frame(width: screenWidth, height: 180, alignment: .center)
     }
 }
 
@@ -70,6 +86,6 @@ struct AddCompanyPopupView_Previews: PreviewProvider {
         let testCompany = CompanyViewModel()
         testCompany.companyList = sampleCompanies
         
-        return AddCompanyPopupView(companyVm: testCompany, showingPopup: .constant(false))
+        return AddCompanyPopupView(companyVm: testCompany, showingPopup: .constant(false), showingAlert: true)
     }
 }
