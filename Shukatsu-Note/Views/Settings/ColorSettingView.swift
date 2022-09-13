4dd5258e1fb74dd9f6f8adabba340ef2d202bfe2
Haruki Koga <harukikoga.dev@gmail.com>
2022-09-13 18:55:28 +0900

    テーマカラーを変更できるよう実装

--- Shukatsu-Note/Views/Settings/ColorSettingView.swift
+++ Shukatsu-Note/Views/Settings/ColorSettingView.swift
@@ -8,8 +8,37 @@
 import SwiftUI
 
 struct ColorSettingView: View {
+    @EnvironmentObject var customColor: CustomColor
+    var colorSet: [String] = [
+        "ThemeColor1", "ThemeColor2", "ThemeColor3", "ThemeColor4", "ThemeColor5", "ThemeColor6"
+    ]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack  {
+            VStack(spacing: 100) {
+                
+                Color(customColor.themeColor)
+                    .frame(width: 230, height: 80)
+                    .cornerRadius(13)
+                
+                VStack {
+                    LazyVGrid(columns: Array(repeating: .init(.flexible(), spacing: 0), count: 3), spacing: 0, content: {
+                        ForEach(0..<colorSet.count) { item in
+                            Button {
+                                customColor.themeColor = colorSet[item]
+                            } label: {
+                                Color(colorSet[item])
+                                    .frame(width: 60, height: 60)
+                                    .clipShape(Circle())
+                                    .padding()
+                            }
+                        }
+                    })
+                }
+                .padding(.horizontal, 30)
+                .navigationTitle("Color Setting")
+            }
+        }
     }
 }
 
