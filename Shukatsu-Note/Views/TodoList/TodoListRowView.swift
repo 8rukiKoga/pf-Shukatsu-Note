bf70508a10178d9caaf227337a4b7598e79cf563
Haruki Koga <harukikoga.dev@gmail.com>
2022-09-13 14:34:21 +0900

    タスク完了時に取り消し線を引くよう実装

--- Shukatsu-Note/Views/TodoList/TodoListRowView.swift
+++ Shukatsu-Note/Views/TodoList/TodoListRowView.swift
@@ -18,40 +18,49 @@ struct TodoListRowView: View {
     
     
     var body: some View {
-
-            HStack {
-                VStack(alignment: .leading) {
-                    if task.date != nil {
-                        HStack {
-                            Text(CustomDateFormatter.shared.convertDateToString(from: task.date!))
-                                .font(.system(size: 12))
-                            if let companyName = task.companyName {
-                                Text("-\(companyName)-")
-                                    .font(.system(size: 10))
-                                    .padding(.leading, 1)
-                            }
+        
+        HStack {
+            VStack(alignment: .leading) {
+                if task.date != nil {
+                    HStack {
+                        Text(CustomDateFormatter.shared.convertDateToString(from: task.date!))
+                            .font(.system(size: 12))
+                        if let companyName = task.companyName {
+                            Text("-\(companyName)-")
+                                .font(.system(size: 10))
+                                .padding(.leading, 1)
                         }
-                        .foregroundColor(.gray)
                     }
-                    
+                    .foregroundColor(.gray)
+                }
+                
+                if task.done {
+                    Text(task.name ?? "")
+                        .strikethrough()
+                        .foregroundColor(.gray)
+                        .padding(.vertical, 1)
+                    // 文字が...で省略されないようにする
+                        .fixedSize(horizontal: false, vertical: true)
+                } else {
                     Text(task.name ?? "")
                         .padding(.vertical, 1)
                     // 文字が...で省略されないようにする
                         .fixedSize(horizontal: false, vertical: true)
                 }
-                Spacer()
-                
-                Button {
-                    withAnimation {
-                        Task.update(in: context, task: task)
-                    }
-                } label: {
-                    Image(systemName: task.done ? "checkmark.circle.fill" : "circle")
-                        .font(task.done ? .title2 : .title3)
-                        .foregroundColor(task.done ? Color(.systemGreen) : .gray)
-                }
             }
-            .padding(.horizontal)
+            Spacer()
+            
+            Button {
+                withAnimation {
+                    Task.update(in: context, task: task)
+                }
+            } label: {
+                Image(systemName: task.done ? "checkmark.circle.fill" : "circle")
+                    .font(task.done ? .title2 : .title3)
+                    .foregroundColor(task.done ? Color(.systemGreen) : .gray)
+            }
+        }
+        .padding(.horizontal)
         
     }
 }
