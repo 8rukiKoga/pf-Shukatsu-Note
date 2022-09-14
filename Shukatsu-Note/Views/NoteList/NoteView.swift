b0736b23e03eb19bbc25a1ae7c860fa8a404dfbf
Haruki Koga <harukikoga.dev@gmail.com>
2022-09-14 11:48:54 +0900

    Model, ViewModelを削除

--- Shukatsu-Note/Views/NoteList/NoteView.swift
+++ Shukatsu-Note/Views/NoteList/NoteView.swift
@@ -10,7 +10,6 @@ import SwiftUI
 struct NoteView: View {
     @Environment(\.managedObjectContext) private var context
     
-    var company: CompanyModel?
     var note: Note
     var companyIndex: Int?
     
