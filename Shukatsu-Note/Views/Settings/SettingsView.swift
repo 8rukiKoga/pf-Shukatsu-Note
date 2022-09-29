8d81fe5ddd66dbfe4dcb5a819d1e2114ceb80449
Haruki Koga <harukikoga.dev@gmail.com>
2022-09-29 14:45:59 +0900

    バージョン変更

--- Shukatsu-Note/Views/Settings/SettingsView.swift
+++ Shukatsu-Note/Views/Settings/SettingsView.swift
@@ -73,7 +73,7 @@ struct SettingsView: View {
                             
                             Spacer()
                             
-                            Text("1.2.2")
+                            Text("1.2.3")
                         }
                         Button {
                             openUrl(url: "https://8rukikoga.github.io/pf-Portfolio/ShukatsuNote.html")
