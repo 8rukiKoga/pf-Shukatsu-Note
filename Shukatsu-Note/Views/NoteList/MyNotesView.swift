ff41ecb4f6ae4f146936949f03e416c3f0f6d8b0
Haruki Koga <harukikoga.dev@gmail.com>
2022-11-12 18:44:05 +0900

    viewの修正

--- Shukatsu-Note/Views/NoteList/MyNotesView.swift
+++ Shukatsu-Note/Views/NoteList/MyNotesView.swift
@@ -52,10 +52,6 @@ struct MyNotesView: View {
                 List {
                     // ノートリスト
                     Section {
-                        Button("notify") {
-                            NotificationManager.instance.scheduleNotification() 
-                        }
-                        
                         // 表示するノートをフィルタリング
                         let globalNotes = notes.filter { $0.companyId == nil }
                         if globalNotes.isEmpty {
