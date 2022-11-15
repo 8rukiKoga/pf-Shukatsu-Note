666d1742d797e6d411896b3dd327304ebe8a1642
Haruki Koga <harukikoga.dev@gmail.com>
2022-11-15 23:15:15 +0900

    NoteRowViewのデザイン変更

--- Shukatsu-Note/Views/Shared/ViewParts/NoteRowView.swift
+++ Shukatsu-Note/Views/Shared/ViewParts/NoteRowView.swift
@@ -14,20 +14,24 @@ struct NoteRowView: View {
     var text: String
     
     var body: some View {
-        
+        ZStack {
+            Color(.systemGray6)
+                .ignoresSafeArea()
+            
             HStack {
                 Image(systemName: "note.text")
                     .foregroundColor(Color(customColor.themeColor))
                     .font(.system(size: 15))
                 
                 Text(text)
-                    .font(.system(size: 15))
+                    .font(.system(size: 11))
                 
                 Spacer()
             }
             .frame(height: 25)
-            .padding(.horizontal, 3)
-            .padding(.vertical, 1)
-        
+            .padding()
+        }
+        .cornerRadius(10)
+        .padding(.vertical, 1)
     }
 }
