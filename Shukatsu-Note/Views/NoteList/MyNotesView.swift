bbb55207ed07728c50e1dead02a55db8dae0ee6e
Haruki Koga <harukikoga.dev@gmail.com>
2022-11-15 23:42:30 +0900

    コード修正

--- Shukatsu-Note/Views/NoteList/MyNotesView.swift
+++ Shukatsu-Note/Views/NoteList/MyNotesView.swift
@@ -67,7 +67,6 @@ struct MyNotesView: View {
                                         NoteRowView(text: note.text ?? "New Note")
                                     }
                                     .cornerRadius(10)
-                                    .padding(.vertical, 1)
                                 }
                             }
                             .onDelete(perform: deleteNote)
